8e2b5701f72fbd4ce1b6b3f1dee0fed042a110d7
Resta5 <muhamadrestha5@gmail.com>
2021-11-26 18:15:02 -0800

    Route Book

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Author;
 use App\Models\Book;
 use Illuminate\Http\Request;
 
@@ -14,7 +15,9 @@ class BookController extends Controller
      */
     public function index()
     {
-        //
+        $book = Book::all();
+        return view('book.index', compact('book'));
+
     }
 
     /**
@@ -24,7 +27,9 @@ class BookController extends Controller
      */
     public function create()
     {
-        //
+        $author = Author::all();
+        return view('book.create', compact('author'));
+
     }
 
     /**
@@ -35,7 +40,27 @@ class BookController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'title' => 'required',
+            'author_id' => 'required',
+            'amount' => 'required',
+            'cover' => 'required',
+        ]);
+
+        $book = new Book;
+        $book->title = $request->title;
+        $book->author_id = $request->author_id;
+// upload image / foto
+        if ($request->hasFile('cover')) {
+            $image = $request->file('cover');
+            $name = rand(1000, 9999) . $image->getClientOriginalName();
+            $image->move('image/book/', $name);
+            $book->cover = $name;
+        }
+        $book->amount = $request->amount;
+        $book->save();
+        return redirect()->route('book.index');
+
     }
 
     /**
@@ -44,9 +69,11 @@ class BookController extends Controller
      * @param  \App\Models\Book  $book
      * @return \Illuminate\Http\Response
      */
-    public function show(Book $book)
+    public function show($id)
     {
-        //
+        $book = Book::findOrFail($id);
+        return view('book.show', compact('book'));
+
     }
 
     /**
@@ -55,9 +82,12 @@ class BookController extends Controller
      * @param  \App\Models\Book  $book
      * @return \Illuminate\Http\Response
      */
-    public function edit(Book $book)
+    public function edit($id)
     {
-        //
+        $book = Book::findOrFail($id);
+        $author = Author::all();
+        return view('book.edit', compact('book', 'author'));
+
     }
 
     /**
@@ -67,9 +97,24 @@ class BookController extends Controller
      * @param  \App\Models\Book  $book
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Book $book)
+    public function update(Request $request, $id)
     {
-        //
+        $book = Book::findOrFail($id);
+        $book->title = $request->title;
+        $book->author_id = $request->author_id;
+        // upload image / foto
+        if ($request->hasFile('cover')) {
+            $book->deleteImage();
+            $image = $request->file('cover');
+            $name = rand(1000, 9999) . $image->getClientOriginalName();
+            $image->move('image/book/', $name);
+            $book->cover = $name;
+        }
+        $book->amount = $request->amount;
+        $book->save();
+
+        return redirect()->route('book.index');
+
     }
 
     /**
@@ -78,8 +123,12 @@ class BookController extends Controller
      * @param  \App\Models\Book  $book
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Book $book)
+    public function destroy($id)
     {
-        //
+        $book = Book::findOrFail($id);
+        $book->deleteImage();
+        $book->delete();
+        return redirect()->route('book.index');
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,3 +24,5 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('author', AuthorController::class);
+
+Route::resource('book', BookController::class);
